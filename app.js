9cab5189b8953c4f0147eddbf7c51d2e0d0a0318
Jennifer Jones <jybjones@gmail.com>
2015-07-20 12:01:13 -0500

    extracted functions into modules

--- app.js
+++ app.js
@@ -4,39 +4,24 @@ var argv = require('yargs')
   // .default('name', 'World')
   .argv;
 var prompt = require('prompt');
+/////import the help.js file here, in essence help becomes the function//
+var help = require('./app/help');
 
 if (argv.help){
-  console.log('intronode');
-  console.log('v1.0.0');
-  console.log('');
-  console.log('usage:');
-  console.log('');
-  console.log('--help             print this message');
-  console.log('--name={name}      input name');
-  console.log('');
-  process.exit(1);
+  help();
+  // help.printHelpMessage();
+  // process.exit(1);
 }
-if (argv.name) {
-  printHelloMessage(argv.name)
-} else {
+//printing msg based on the argv statement/////
+  prompt.override = argv;
   prompt.message = prompt.delimiter = '';
   prompt.start();
 
   prompt.get('name', function (err, result) {
     printHelloMessage(result.name);
-
   });
-}
 
 function printHelloMessage(name) {
   console.log('Hello ' + name);
   process.stdout.write('Hello ' + name + ' Again!\n');
 }
-
-// console.error('ooops');
-// process.stderr.write('OMG HELP!!\n');
-
-
-// console.log(process.argv);
-// console.log(argv);
-// }
